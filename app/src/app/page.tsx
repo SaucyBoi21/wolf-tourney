1e6f7e43da28d0822d377492935976d2080cdbe3
Saahas Swaroop <saahas.swaroop@googlemail.com>
2025-03-18 22:55:36 -0400

    router and navbar

---
+++ app/src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Navbar from "./components/navbar"
+import { useState } from "react"
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
