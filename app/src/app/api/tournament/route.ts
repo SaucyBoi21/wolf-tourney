f2ac86054760fea626b942e8a96c6b32f7b5583b
Saahas Swaroop <saahas.swaroop@googlemail.com>
2025-03-27 19:30:28 -0400

    backend

--- app/src/app/api/tournament/route.ts
+++ app/src/app/api/tournament/route.ts
@@ -1,19 +1,39 @@
+import { NextResponse, NextRequest } from "next/server"
+import { PrismaClient, Prisma } from "@prisma/client"
+
+const prisma = new PrismaClient
+
 // POST create new tournament
 export async function POST(request:Request) {
 
     const body = await request.json()
-    const {name, rounds, status} = body
 
-    const newTournament = {
-        "name": name,
-        "rounds": rounds,
-        "status": status
+    const {name, rounds, status, sections} = body
+
+
+    
+    
+
+    try {
+
+        const newTournament = prisma.tournament.create({
+            data: {
+                name: name,
+                rounds: rounds,
+                sections: sections,
+            }
+        })
+
+        return new Response(JSON.stringify(newTournament), {
+            status: 201,
+            headers: {'Content-Type': 'application/json'}
+        })
+
     }
 
-    return new Response(JSON.stringify(newTournament), {
-        status: 201,
-        headers: {'Content-Type': 'application/json'}
-    })
+    catch (error) {
+        return new Response(error)
+    }
     
 }
 
